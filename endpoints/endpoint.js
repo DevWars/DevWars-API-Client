4bf4401bd786d71523c9f5619331f636c20cb582
Stephen Lineker-Miller <slinekermiller@gmail.com>
2020-05-26 16:48:49 +0100

    maint: body is now applied to url on get requests

--- endpoints/endpoint.js
+++ endpoints/endpoint.js
@@ -1,68 +1,98 @@
 const ApiError = require("./api.error");
 
 module.exports = class Endpoint {
-    /**
-     * Creates a endpoint interface to be implemented.
-     * @param {AxiosInstance} client The axios client to be used to communicate with the api.
-     * @param {string} path Any additional appended path (e.g /users)
-     */
-    constructor(client, path) {
-        this.client = client;
-        this.path = path;
+  /**
+   * A static body that will be joined with any given body that is also sent.
+   * This can be used to sent api tokens.
+   */
+  static body = {};
+
+  /**
+   * Creates a endpoint interface to be implemented.
+   * @param {AxiosInstance} client The axios client to be used to communicate with the api.
+   * @param {string} path Any additional appended path (e.g /users)
+   */
+  constructor(client, path) {
+    this.client = client;
+    this.path = path;
+  }
+
+  /**
+   * Build up a request that will be sent to the server.
+   * @param {object} options The related options to the endpoint call.
+   */
+  buildOptions(options) {
+    this.validateRequestContent(options);
+    return options;
+  }
+
+  /**
+   * All options must be specified before making a call
+   * @param {object} options The options for the request.
+   */
+  validateRequestContent(options) {
+    ["method", "path", "body"].forEach((option) => {
+      if (!options[option]) {
+        throw new Error(`${option} has to be specified`);
+      }
+    });
+  }
+
+  /**
+   * Gets the body content based on the method, (returned stringify for json
+   * server expectation)
+   * @param {string} method The method of the request.
+   * @param {object} body The body to be sent.
+   */
+  getBodyContent(method, body) {
+    return ["get", "head"].includes(method.toLowerCase())
+      ? undefined
+      : Object.assign({}, Endpoint.body, body);
+  }
+
+  /**
+   * Gets the url for the given request. If this is a get request, and the
+   * extra body parameters have been specified, then bind them to the url.
+   * server expectation)
+   * @param {string} method The method of the request.
+   * @param {string} url The raw url without any changes.
+   */
+  getUrl(method, url) {
+    if (["get", "head"].includes(method.toLowerCase())) return url;
+
+    const queryParams = [];
+
+    for (const key in Endpoint.body) {
+      queryParams.push(`${key}=${Endpoint.body[key]}`);
     }
 
-    /**
-     * Build up a request that will be sent to the server.
-     * @param {object} options The related options to the endpoint call.
-     */
-    buildOptions(options) {
-        this.validateRequestContent(options);
-        return options;
-    }
+    return `${url}?${queryParams.join("&")}`;
+  }
 
-    /**
-     * All options must be specified before making a call
-     * @param {object} options The options for the request.
-     */
-    validateRequestContent(options) {
-        ["method", "path", "body"].forEach((option) => {
-            if (!options[option]) {
-                throw new Error(`${option} has to be specified`);
-            }
-        });
-    }
+  /**
+   * Performs the api call with the fetch operator based on the options.
+   * @param {object} options The options for the request.
+   */
+  async apiCall(options) {
+    try {
+      const body = this.getBodyContent(options.method, options.body);
+      const url = this.getUrl(options.method, options.path);
 
-    /**
-     * Gets the body content based on the method, (returned stringify for json
-     * server expectation)
-     * @param {string} method The method of the request.
-     * @param {object} body The body to be sent.
-     */
-    getBodyContent(method, body) {
-        return ["get", "head"].includes(method.toLowerCase())
-            ? undefined
-            : Object.assign({}, DevWarsApi.body, body);
-    }
+      console.log(url, body);
 
-    /**
-     * Performs the api call with the fetch operator based on the options.
-     * @param {object} options The options for the request.
-     */
-    async apiCall(options) {
-        try {
-            const body = this.getBodyContent(options.method, options.body);
-            const result = await this.client[options.method](
-                options.path,
-                body
-            );
+      const result = await this.client[options.method](url, body);
 
-            return result.data;
-        } catch (error) {
-            const response = error.response.data;
-            throw new ApiError(
-                error.response.status,
-                response.error || "Something went wrong."
-            );
-        }
+      return result.data;
+    } catch (error) {
+      if (error.response == null || error.response.data == null) {
+        throw new ApiError(-1, error.message || "Something went wrong.");
+      }
+
+      const response = error.response.data;
+      throw new ApiError(
+        error.response.status,
+        response.error || "Something went wrong."
+      );
     }
+  }
 };
--- index.js
+++ index.js
@@ -2,19 +2,14 @@ const LinkedAccounts = require("./endpoints/linkedAccounts");
 const Authentication = require("./endpoints/authentication");
 const Leaderboards = require("./endpoints/leaderboards");
 const Activities = require("./endpoints/activities");
+const Endpoint = require("./endpoints/endpoint");
 const Contact = require("./endpoints/contact");
 const Health = require("./endpoints/health");
 const Search = require("./endpoints/search");
 const Users = require("./endpoints/users");
 const Games = require("./endpoints/games");
 
-module.exports = class DevWarsApi {
-  /**
-   * A static body that will be joined with any given body that is also sent.
-   * This can be used to sent api tokens.
-   */
-  static body;
-
+class DevWarsApi {
   /**
    * Creates a new instance of the devwars api.
    * @param {AxiosInstance} client The axios client to be used to communicate with the api.
@@ -30,4 +25,14 @@ module.exports = class DevWarsApi {
     this.users = new Users(axios, "users");
     this.games = new Games(axios, "games");
   }
-};
+
+  /**
+   * A static body that will be joined with any given body that is also sent.
+   * This can be used to sent api tokens.
+   */
+  set body(value) {
+    Endpoint.body = value;
+  }
+}
+
+module.exports = DevWarsApi;
